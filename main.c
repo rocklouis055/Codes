1da068760a71982ff483eca16fda3794a68e2f7f
Louise Patra <85580027+rocklouis055@users.noreply.github.com>
2022-04-01 23:46:27 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,21 @@
+/* Programme 13:
+Q. Implement structures to read, write, compute average marks and the students scoring above and below the average marks for a class of N students.
+Louise Patra
+1CR20CS103
+*/
+#include<stdio.h>
+int main(){
+struct student{
+  int stid;
+  char name[20];
+  float m1;
+  float m2;
+};
+struct s2{
+  struct student b;
+  int a;
+}
+struct student a[50];
+}
+return(0);
+}
