ff752927ba4fab5c593dfe0506d4bca4a5d771b2
Louise Patra <85580027+rocklouis055@users.noreply.github.com>
2022-04-02 21:47:26 +0530

    Update dfs and bfs

--- 11.c
+++ 11.c
@@ -1,11 +1,29 @@
 #include<stdlib.h>
 #include<stdio.h>
-void dfs(){
-  
+
+void dfs(int *g,int *v,int c,int n){
+  printf(" %d",c);
+  v[c]=1;
+  for(int i=0;i<n;i++)
+    if(*(g+c*n+i) && !v[i])
+      dfs(g,v,i,n);
 }
 
-void bfs(){
-  
+void bfs(int *g,int *v,int c,int n){
+  int f=-1,r=0;
+  int q[n];
+  printf(" %d",c);
+  q[r++]=c;
+  v[c]=1;
+  while(r>f){
+    for(int i=0;i<n;i++)
+      if(*((g+c*n)+i) && !v[i]){
+        v[i]=1;
+        printf(" %d",i);
+        q[r++]=i;
+      }
+    c=q[++f];
+  }
 }
 
 int main(){
@@ -13,10 +31,16 @@ int main(){
   printf("Enter Number of elements in the Graph : ");
   scanf("%d",&n);
   int g[n][n],v[n];
-  printf("Enter the graph adjacency matrix %dx%d : ",n,n);
-  for (int i=0;i<n;i++){
-    for (int j=0;j<n;j++){
-      scanf("%d",g[i][j]);
-    }
-  }
+  for(int i=0;i<n;v[i]=0,i++);
+  printf("Enter the graph adjacency matrix %dx%d :\n",n,n);
+  for (int i=0;i<n;i++)
+    for (int j=0;j<n;j++)
+      scanf("%d",&g[i][j]);
+  printf("\nThe DFS order of the graph is :");
+  dfs(*g,v,0,n);
+  for(int i=0;i<n;v[i]=0,i++);
+  printf("\nThe BFS order of the graph is :");
+  bfs(*g,v,0,n);
+  printf("\n");
+  return(0);
 }
