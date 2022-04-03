95ef0e3babe7d6bacd716ba1167bf5229b1e7106
Louise Patra <85580027+rocklouis055@users.noreply.github.com>
2022-04-03 18:20:48 +0530

    Added Example

--- 11.c
+++ 11.c
@@ -44,3 +44,32 @@ int main(){
   printf("\n");
   return(0);
 }
+
+/*
+Example :
+  Enter Number of elements in the Graph : 5
+  Enter the graph adjacency matrix 5x5 :
+  0 1 1 1 0 
+  1 0 1 0 0 
+  1 1 0 0 1 
+  1 0 0 0 0
+  0 0 1 0 0
+  The DFS order of the graph is : 0 1 2 4 3
+  The BFS order of the graph is : 0 1 2 3 4
+
+  Enter Number of elements in the Graph : 10
+  Enter the graph adjacency matrix 10x10 :
+  0 1 1 1 0 0 0 0 0 0
+  1 0 0 0 1 1 0 0 0 0 
+  1 0 0 0 0 0 1 1 0 0 
+  1 0 0 0 0 0 0 0 1 1 
+  0 1 0 0 0 0 0 0 0 0 
+  0 1 0 0 0 0 0 0 0 0 
+  0 0 1 0 0 0 0 0 0 0 
+  0 0 1 0 0 0 0 0 0 0 
+  0 0 0 1 0 0 0 0 0 0 
+  0 0 0 1 0 0 0 0 0 0
+  
+  The DFS order of the graph is : 0 1 4 5 2 6 7 3 8 9
+  The BFS order of the graph is : 0 1 2 3 4 5 6 7 8 9
+*/
